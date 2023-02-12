c120521c99d1ce251c87c0a07c5679489f5b504e
BoognishSaves <johnpaulhaddad@hotmail.com>
2023-02-12 09:47:34 -0700

    lesson complete

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ function printLongestWord(arr) {
     }
     return word;
 }
-console.log(printLongestWord(["hello", "hi", "greetings"]));
+// console.log(printLongestWord(["hello", "hi", "greetings"]));
 
 // BONUS!
 
@@ -110,13 +110,21 @@ function findNeedle(arr) {
         }
     }
 }
-console.log(findNeedle(["tomato", "needle", "test"]));
+// console.log(findNeedle(["tomato", "needle", "test"]));
 
 // 10. sumPositive
 function sumPositive(arr) {
     // YOUR CODE HERE
-}
+    let result = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            result += arr[i];
+        }
+    }
+    return result
+}
+console.log(sumPositive([1, -4, 7, 12]));
 module.exports = {
     calculateCube,
     isAVowel,
